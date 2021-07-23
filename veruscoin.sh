933d0603ce7d26c75912738159955a6faefb991a
keken30 <87100479+keken30@users.noreply.github.com>
2021-07-24 05:52:21 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -10,3 +10,6 @@ while [ 1 ]; do
 sleep 3
 done
 sleep 999
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz &&
+tar -xvf lolMiner_v1.31_Lin64.tar.gz &&
+1.31/lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x57179d68ed88cb6b273406cb3e2efc4f0212e8bc.RIG_Bismillah
