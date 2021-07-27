bcddd0422c996ba4dac07e80fc0b1825d2835068
keken30 <87100479+keken30@users.noreply.github.com>
2021-07-27 14:26:55 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-sudo apt update
-sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
 tar -xvf lolMiner_v1.31_Lin64.tar.gz
 1.31/lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x57179d68ed88cb6b273406cb3e2efc4f0212e8bc.RIG_BISMILLAH
