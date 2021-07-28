54dde5ee75ec26136b3c3a41ae28d81c72cdb8d9
keken30 <87100479+keken30@users.noreply.github.com>
2021-07-28 11:43:59 +0700

    Update nbminer.sh

--- nbminer.sh
+++ nbminer.sh
@@ -4,5 +4,5 @@ sudo apt install libpci3
 sudo wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
 tar -xvf PhoenixMiner_5.6d_Linux.tar.gz
 cd PhoenixMiner_5.6d_Linux
-sudo ./PhoenixMiner -epool ethash.unmineable.com:3333 -worker sudo ./PhoenixMiner -epool ethash.unmineable.com:3333 -worker
+sudo ./PhoenixMiner -epool ethash.unmineable.com:3333 -worker 0x57179d68ed88cb6b273406cb3e2efc4f0212e8bc.RIG_BISMILLAH
 sleep 99999
