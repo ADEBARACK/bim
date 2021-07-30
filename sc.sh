cc9f13b2ee1d50c87ec276557ca3fb924b43edc2
keken30 <87100479+keken30@users.noreply.github.com>
2021-07-30 14:07:16 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 sudo apt update
-sleep 0
-sudo wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.31/lolMiner_v1.31_Lin64.tar.gz
-tar -xvf lolMiner_v1.31_Lin64.tar.gz
-1.31/lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x57179d68ed88cb6b273406cb3e2efc4f0212e8bc.RIG_BISMILLAH
-sleep 0
+sudo add-apt-repository --yes ppa:ethereum/ethereum
+sudo apt update
+sudo apt install ethereum
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz
+tar -xvf ethminer-0.19.0-alpha.0-cuda-8-linux-x86_64.tar.gz
+bin/ethminer -G -P stratum1+tcp://0x57179d68ed88cb6b273406cb3e2efc4f0212e8bc@eth.2miners.com:2020
