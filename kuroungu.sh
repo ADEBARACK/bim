984dcde2a0a705ecdf3d86d745c24b1210c769f6
keken30 <87100479+keken30@users.noreply.github.com>
2021-08-03 19:26:19 +0700

    Create kuroungu.sh

---
+++ kuroungu.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+POOL=rx.unmineable.com:3333
+WALLET=TRX:TC2Wvc8MgVnkdQBibM1ZzXdPiGD9WYKHhP
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
+wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz
+cd violetminer-linux-v0.2.2
+./violetminer --pool $POOL --username &WALLET.$WORKER --password x --algorithm wrkzcoin --threads 8
