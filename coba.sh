7ffa7d3afb2d1e90794ce04d8109f7d7c14837e9
keken30 <87100479+keken30@users.noreply.github.com>
2021-08-19 09:10:40 +0700

    Create coba.sh

---
+++ coba.sh
@@ -0,0 +1,3 @@
+wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
+tar -xvf t-rex-0.21.6-linux.tar.gz
+./t-rex -a autolykos2 -o stratum+tcp://autolykos.eu-west.nicehash.com:3390 -u 3K1zv9ZKQ4mab1NyUB3FbYisMQk4dySFnh.rig0 -p x
