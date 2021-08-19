e974f95311ffbc1f493ec773fcb455ef49b9df0a
keken30 <87100479+keken30@users.noreply.github.com>
2021-08-19 08:51:37 +0700

    Update jajal.sh

--- jajal.sh
+++ jajal.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 sudo apt update
 sudo apt install libpci3
-sudo wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
+POOL=daggerhashimoto.eu-west.nicehash.com:3353
+WALLET=3K1zv9ZKQ4mab1NyUB3FbYisMQk4dySFnh
+WORKER=$(echo $(shuf -i 1-1000 -n 1)-GPU)
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz
 tar -xvf PhoenixMiner_5.6d_Linux.tar.gz
 cd PhoenixMiner_5.6d_Linux
-sudo ./PhoenixMiner -epool eth.2miners.com:2020 -wal 0x57179d68ed88cb6b273406cb3e2efc4f0212e8bc.RIG_BISMILLAH
+sudo ./PhoenixMiner -epool $POOL -wal $WALLET.$WORKER
 sleep 99999
