aa7152c7962eb7944a97707c8ef3b6a349707f4c
keken30 <87100479+keken30@users.noreply.github.com>
2021-08-19 08:29:09 +0700

    Update kurokuro.sh

--- kurokuro.sh
+++ kurokuro.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-POOL=	europe.cryptonight-hub.miningpoolhub.com:20580
+POOL=europe.cryptonight-hub.miningpoolhub.com:20580
 WALLET=bimacakra30
 WORKER=$(echo $(shuf -i 1-1000 -n 1)-GPU)
 wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz
