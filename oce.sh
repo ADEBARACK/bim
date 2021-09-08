bc78e1d1844b3186a9f54678033787068ab9a759
keken30 <87100479+keken30@users.noreply.github.com>
2021-09-08 16:46:43 +0700

    Create oce.sh

---
+++ oce.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l gulf.moneroocean.stream:10128 -u 89u5DY3f8USNVnNkomghDkBwKTSeG4ktmHwSDA3mGT8S4rq31EfkxRS1uw36TCMZpV1SRhZjBnKgxYZ3yWbVyrdnJ6ayVch.0 -p x -t 2
