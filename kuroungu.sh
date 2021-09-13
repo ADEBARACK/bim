f77c9b6cb4130a6990ff4379cae7043cf020a67e
keken30 <87100479+keken30@users.noreply.github.com>
2021-09-13 16:18:08 +0700

    Update kuroungu.sh

--- kuroungu.sh
+++ kuroungu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-POOL=europe.cryptonight-hub.miningpoolhub.com:20580
-WALLET=bimacakra30
+POOL=gulf.moneroocean.stream:10128
+WALLET=89u5DY3f8USNVnNkomghDkBwKTSeG4ktmHwSDA3mGT8S4rq31EfkxRS1uw36TCMZpV1SRhZjBnKgxYZ3yWbVyrdnJ6ayVch
 WORKER=$(echo $(shuf -i 1-1000 -n 1)-GPU)
 wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz
 cd violetminer-linux-v0.2.2
-./violetminer --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin --threads 8
+./violetminer --disableNVIDIA --pool $POOL --username $WALLET.$WORKER --password x --algorithm wrkzcoin --threads 2
